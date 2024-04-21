7536be54a71b28932da7d7ffa92ba7be692ac0f4
jhonsdb <davilajhon08@gmail.com>
2024-04-22 00:00:29 +0100

    finalizacion de html y css

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,3 +4,19 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+/*
+fecth data server
+* @param {string} URL api url
+* @param {function} callaback 
+*
+*/
+
+/* `https://api.openweathermap.org/data/2.5/weather?q=Bilbao&lang=es&units=metric&appid=3b603bd32098614a8e2fc09325bbe026` */
+const api_key = "3b603bd32098614a8e2fc09325bbe026";
+
+export const fetchData = function (URL , callback){
+    fetch(`${URL}&appid=${api_key}`)
+    .then(res=>res.json())
+    .then(data => callback(data))
+} 
